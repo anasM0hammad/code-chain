205b2999039723c90ad635d00237877baa4381ca
modkhalid <modkhalid5@gmail.com>
2018-10-13 23:02:21 +0530

    index file modified according to common directory

--- home/index.php
+++ home/index.php
@@ -2,15 +2,7 @@
 <html>
 <head>
 	<title>Code Chain | Home</title>
-	<meta charset="utf-8">
-  	<meta name="viewport" content="width=device-width, initial-scale=1">
-  	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
-  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
-  	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-  	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
-  	<script src="js/script.js" ></script>
-	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+	<?php include '../common/common-code.php';topHeader();?>
 	<link rel="stylesheet" type="text/css" href="../css/main.css">
 	<script src="../js/main.js"></script>
   <style type="text/css">
